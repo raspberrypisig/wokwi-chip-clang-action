709bdfef62499c69220dfc96d83495720e508dbd
raspberrypisig <raspberrypisig@gmail.com>
2025-09-08 11:41:18 +1000

    make output wasm name configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p dist
-clang --target=wasm32-unknown-wasi --sysroot /opt/wasi-libc -nostartfiles -Wl,--export-table -Wl,--no-entry -Werror -o dist/chip.wasm $SOURCES
+clang --target=wasm32-unknown-wasi --sysroot /opt/wasi-libc -nostartfiles -Wl,--export-table -Wl,--no-entry -Werror -o dist/${OUTPUT_NAME}.wasm $SOURCES
